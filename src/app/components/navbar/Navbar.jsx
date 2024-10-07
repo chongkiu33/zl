6f89ef56490d905a803852cb1ad8c0e1960e9d69
chongkiu33 <chongkiu33@users.noreply.github.com>
2024-10-07 20:43:01 +0800

    2234

--- src/app/components/navbar/Navbar.jsx
+++ src/app/components/navbar/Navbar.jsx
@@ -5,8 +5,11 @@ const Navbar = () => {
     return (
         <div className={styles.container}>
             <div>Zhuolin Li</div>
-            <div><Link href="/works">Works</Link></div>
-            <div><Link href="/playground">Playground</Link></div>
+            <div className={styles.wAndp}>
+                <Link href="/works">Works</Link>
+                <Link href="/playground">Playground</Link>
+            </div>
+            
             <div><Link href="/about">About</Link></div>
         </div>
     )
