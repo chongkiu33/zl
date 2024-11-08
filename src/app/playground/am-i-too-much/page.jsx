d679a8b48676f90bca94b0d5cf537715a2cd6d13
chongkiu33 <chongkiu33@users.noreply.github.com>
2024-11-08 14:27:42 +0800

    change

--- src/app/playground/am-i-too-much/page.jsx
+++ src/app/playground/am-i-too-much/page.jsx
@@ -17,11 +17,13 @@ const Page = () => {
             <div className={styles.spChapterText}>
             
             <TextSplitter text={`“Am I too much?” is an interactive novel about the nuance and subtleness in conversations. The player could manipulate the punctuations to mimic the different tones. In repeating the conversations and reflections, I want to recreate an obsessive procedure that continuesly guessing what does the other really mean while talking.`}/>
-            “Am I too much?” is an interactive novel about the nuance and subtleness in conversations. The player could manipulate the punctuations to mimic the different tones. In repeating the conversations and reflections, I want to recreate an obsessive procedure that continuesly guessing what does the other really mean while talking.
-            <br/><br/>
-            Is “No, I’m just not enough” a good answer to “Am I too much?” Or shall I reply with “No! I’m just not enough!” Is he/she/they just conversing out of pleasantries or keeping appearances? Is the words of affirmation atucally an affirmation? We probably will never know.
-            <br/><br/>
-            Does communication really transmit what we want or the wording is pale? In rumination, comtemplation, and tossed and turned nights, I’m seeking for the answer and experiencing, trying to feel the world, others, and myself.
+            
+            
+            <TextSplitter text={`Is “No, I’m just not enough” a good answer to “Am I too much?” Or shall I reply with “No! I’m just not enough!” Is he/she/they just conversing out of pleasantries or keeping appearances? Is the words of affirmation atucally an affirmation? We probably will never know.`}/>
+            
+           
+            <TextSplitter text={`Does communication really transmit what we want or the wording is pale? In rumination, comtemplation, and tossed and turned nights, I’m seeking for the answer and experiencing, trying to feel the world, others, and myself.`}/>
+            
             </div>
         </div>
         <div className='bottomspace'></div>
--- src/app/playground/page.jsx
+++ src/app/playground/page.jsx
@@ -3,6 +3,7 @@ import styles from './pg.module.css';
 import Image from 'next/image';
 import { motion } from 'framer-motion';
 import Link from 'next/link';
+import TextSplitter from '../components/TextSplitter/TextSplitter'
 
 
 
@@ -22,11 +23,15 @@ const plagroundPage = () => {
         <div className={styles.spChapter}>
             <div className={styles.spChapterTitle}>Introduction</div>
             <div className={styles.spChapterText}>
-            “Am I too much?” is an interactive novel about the nuance and subtleness in conversations. The player could manipulate the punctuations to mimic the different tones. In repeating the conversations and reflections, I want to recreate an obsessive procedure that continuesly guessing what does the other really mean while talking.
-            <br/><br/>
-            Is “No, I’m just not enough” a good answer to “Am I too much?” Or shall I reply with “No! I’m just not enough!” Is he/she/they just conversing out of pleasantries or keeping appearances? Is the words of affirmation atucally an affirmation? We probably will never know.
-            <br/><br/>
-            Does communication really transmit what we want or the wording is pale? In rumination, comtemplation, and tossed and turned nights, I’m seeking for the answer and experiencing, trying to feel the world, others, and myself.
+            
+            <TextSplitter text={`“Am I too much?” is an interactive novel about the nuance and subtleness in conversations. The player could manipulate the punctuations to mimic the different tones. In repeating the conversations and reflections, I want to recreate an obsessive procedure that continuesly guessing what does the other really mean while talking.`}/>
+            
+            
+            <TextSplitter text={`Is “No, I’m just not enough” a good answer to “Am I too much?” Or shall I reply with “No! I’m just not enough!” Is he/she/they just conversing out of pleasantries or keeping appearances? Is the words of affirmation atucally an affirmation? We probably will never know.`}/>
+            
+           
+            <TextSplitter text={`Does communication really transmit what we want or the wording is pale? In rumination, comtemplation, and tossed and turned nights, I’m seeking for the answer and experiencing, trying to feel the world, others, and myself.`}/>
+            
             </div>
         </div>
         <div className='bottomspace'></div>
