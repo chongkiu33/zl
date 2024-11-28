ba43f9a333a04bac8c2174ef73688a5eaf8d28cc
chongkiu33 <chongkiu33@users.noreply.github.com>
2024-11-28 17:19:35 +0800

    change

--- src/app/playground/window-frames/page.jsx
+++ src/app/playground/window-frames/page.jsx
@@ -1,10 +1,72 @@
+"use client"
 import styles from '../pg.module.css'
 import Image from 'next/image'
 import Link from 'next/link'
 import TextSplitter from '../../components/TextSplitter/TextSplitter'
 import ProjectTags from '../../components/ProjectTag/ProjectTag'
+import * as THREE from 'three';
+import { OrbitControls } from '@react-three/drei';
+import { Canvas , useFrame , useLoader } from '@react-three/fiber';
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
+import { useRef } from "react";
+
 
 const Page = () => {
+        const groupRef = useRef();
+
+        const objects = [
+                {
+                  objPath: "/window/138.obj",
+                  position: [2, 0, 0],
+                  scale: [0.4, 0.4, 0.4],
+                  rotation: [0, Math.PI / 2, 0],
+                },
+                {
+                  objPath: "/window/139.obj",
+                  position: [-2, 0, 0],
+                  scale: [0.4, 0.4, 0.4],
+                  rotation: [0, -Math.PI / 2, 0],
+                },
+              ];
+
+        function Scene({ objects, color = "grey"  }) {
+
+                useFrame(() => {
+                        if (groupRef.current) {
+                          groupRef.current.rotation.y += 0.01; // 每帧绕 Y 轴旋转
+                        }
+                      });
+
+                return (
+                        <group ref={groupRef}>
+                          {objects.map((objData, index) => {
+                            const obj = useLoader(OBJLoader, objData.objPath);
+                    
+                            // 为每个模型添加颜色
+                            obj.traverse((child) => {
+                              if (child.isMesh) {
+                                child.material = new THREE.MeshStandardMaterial({ color });
+                              }
+                            });
+                    
+                            return (
+                              <group
+                                key={index}
+                                position={objData.position}
+                                scale={objData.scale}
+                                rotation={objData.rotation}
+                              >
+                                <primitive object={obj} />
+                              </group>
+                            );
+                          })}
+                        </group>
+                      );
+        }
+
+     
+
+
   return <div>
     <div className={styles.spTitle}>Window Frames For Ancient China Pavilion</div>
     <ProjectTags projectName="Window Frames For Ancient China Pavilion" />
@@ -19,8 +81,31 @@ const Page = () => {
 
             <TextSplitter text={`This project remodel the window frames design in Ming Dynasty based on The Craft of Gardens(園治). This is part of Zhuolin Li’s dedication for digitalizing the cultural heritages. `}/>
             <TextSplitter text={`One of the illustrations from the book The Craft of Gardens(園治)`}/>
-            <Link className={styles.link} href="chrome-extension://efaidnbmnnnibpcajpcglclefindmkaj/https://ia600703.us.archive.org/12/items/02090253.cn/02090253.cn.pdf">Yuanzhi</Link>
-            <Link className={styles.link} href="chrome-extension://efaidnbmnnnibpcajpcglclefindmkaj/https://ia600703.us.archive.org/12/items/02090253.cn/02090253.cn.pdf">The digital archive</Link>
+            <Link className={styles.link} target="_blank"  href="chrome-extension://efaidnbmnnnibpcajpcglclefindmkaj/https://ia600703.us.archive.org/12/items/02090253.cn/02090253.cn.pdf">Yuanzhi</Link>
+            <Link className={styles.link} target="_blank"  href="chrome-extension://efaidnbmnnnibpcajpcglclefindmkaj/https://ia600703.us.archive.org/12/items/02090253.cn/02090253.cn.pdf">The digital archive</Link>
+            </div>     
+    </div>
+
+    <div className={styles.spChapter}>
+            <div className={styles.spChapterTitle}>Modeling</div>
+            <div className={styles.spChapterText} >
+                
+
+        <Canvas camera={{ position: [0, 0, 10] }} style={{height: '500px'}}>
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[10, 10, 10]} />
+        
+        
+       
+        <Scene objects={objects} />
+
+
+                
+              
+      
+        <OrbitControls />
+        </Canvas>
+
             </div>     
     </div>
 
