e7e434b0b24277613df994f405c490cb3b1fd47f
chongkiu33 <chongkiu33@users.noreply.github.com>
2024-11-30 22:06:00 +0800

    change

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import {motion} from "framer-motion";
-import Intro from "../components/Intro/Intro";
+import Intro from "../components/intro/Intro";
 import { useRouter } from "next/navigation";
 import {useState, useEffect } from "react";
 
