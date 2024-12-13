432d7257292ff73069f0dabf459b3b83be17f9c4
chongkiu33 <chongkiu33@users.noreply.github.com>
2024-12-14 00:58:34 +0800

    change

--- src/app/components/animation/SlideInFromBtn.jsx
+++ src/app/components/animation/SlideInFromBtn.jsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 const SlideInFromBtn = ({ children, delay = 0 }) => {
   return (
     <motion.div
-      initial={{ y: '20%' ,opacity: 0 }}  // 初始位置在视口右侧
+      initial={{ y: '50px' ,opacity: 0 }}  // 初始位置在视口右侧
       whileInView={{ y: 0,opacity: 1 }}     // 进入视口时，元素滑到原位
       transition={{
         type: 'spring',
