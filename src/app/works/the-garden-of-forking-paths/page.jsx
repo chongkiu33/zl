b245cec6adee5390d6a0ac100fa0129b14370727
chongkiu333 <xingshanhe33@gmail.com>
2024-12-28 14:12:31 +0800

    change

--- src/app/works/the-garden-of-forking-paths/page.jsx
+++ src/app/works/the-garden-of-forking-paths/page.jsx
@@ -42,6 +42,10 @@ const Page = () => {
             
         </div>
 
+        <div className={styles.abstract}>
+        This project aims to create an infinite storyline (narration frame) in Twine based on the novel “El jardín de senderos que se bifurcan” (The Garden of Forking Paths) by Jorge Luis Borges. That is, a narration either loops (and grows), or includes all the possibilities at the same time (i.e., the player presumably could choose all the branches). Those different storylines will converge, diverge, and parallel, co-exist in the story.
+        </div>
+
         
         <SlideInFromBtn>
         <div className={styles.pChapter}>
